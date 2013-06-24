0b9607a43dea31e343ff1fb897d9e9aeb957934c
twer <tj19832@gmail.com>
2013-06-24 17:42:35 +0800

    jtong - add comment for active_baoming_controller.js

--- www/js/controller/active_baoming_controller.js
+++ www/js/controller/active_baoming_controller.js
@@ -3,6 +3,7 @@ function active_baoming_controller($scope,$navigate)
     $scope.people_number=JSON.parse(localStorage.getItem('people_number'));
     $scope.flag='开始';
     localStorage.setItem('nname',$scope.flag);
+    //既然能做开始和结束,为什么函数叫begin?叫switch还靠谱点.而且最后那个 return;也显得非常诡异.后面都没有代码了 return什么?
     $scope.begin=function()
     {
         if($scope.flag=='开始')
@@ -35,14 +36,20 @@ function active_baoming_controller($scope,$navigate)
     {
         $navigate.go('/active_jingjia');
     }
+
+    //return? 这里是要起到什么效果?
     if(localStorage.getItem('name')==null)
     {
         return;
     }
     $scope.peoples=JSON.parse(localStorage.getItem('people_temp'));
+
+    //temp是个什么函数?永远不要出现这种函数名,让机器能读懂的代码谁都会写,要写人能读懂的代码.
     $scope.temp=function()
     {
         console.log('已执行');
+        //初始化的工作应该统一完成,比如在document ready里.这样就不用四处写重复代码了.而且看着都很怪.
+        //初始化的逻辑混杂在业务逻辑里
         $scope.people_number=JSON.parse(localStorage.getItem('people_number'));
         if($scope.peoples.length==0)
         {
@@ -54,6 +61,7 @@ function active_baoming_controller($scope,$navigate)
         }
         localStorage.setItem('people_temp',JSON.stringify($scope.peoples));
     }
+    //怪怪的,为什么要判断一下?
     if($scope.peoples.length==0)
     {
         $scope.peoples=[{name:localStorage.getItem('name'),number:localStorage.getItem('number')}]
